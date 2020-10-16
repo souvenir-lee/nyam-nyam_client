2e1bdaba1382bb3703f030e657a0282c7b30b244
lwd3737 <lwd3737@naver.com>
2020-10-16 18:11:21 +0900

    update

--- src/navigation/signup.tsx
+++ src/navigation/signup.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignupParamList } from '../types';
 import Signup from '../screens/Signup';
-import SignUpAddress from '../screens/SignUpAddress';
+//import StoreRegister from '../screens/StoreRegister';
 
 const SignupStack = createStackNavigator<SignupParamList>();
 
-export default function SignupStackNavigation() {
-  return (
-    <SignupStack.Navigator>
-      <SignupStack.Screen name="Signup" component={Signup} />
-      <SignupStack.Screen name="SignUpAddress" component={SignUpAddress} />
-    </SignupStack.Navigator>
-  );
+import { View, Text } from 'react-native';
+function StoreRegister(){
+    return (
+        <View>
+            <Text>
+                Store Register
+            </Text>
+        </View>
+    )
+}
+
+export default function SignupStackNavigation(){
+    return (
+        <SignupStack.Navigator>
+            <SignupStack.Screen name="Signup" component={Signup} />
+            <SignupStack.Screen name="StoreRegister" component={StoreRegister} />
+        </SignupStack.Navigator>
+    )
 }
